13d68daba1321e156f77ae47d2c5e89a235d1669
Juuso Parkkinen <juuso.parkkinen@iki.fi>
2022-12-02 08:30:05 +0000

    version 1.0.6

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# helsinki - Helsinki open data R tools
+# helsinki - Helsinki open data R tools <a href="https://ropengov.github.io/helsinki/"><img src="man/figures/logo.png" align="right" height="139"/></a>
 
 <!-- badges: start -->
 
+[![rOG-badge](https://ropengov.github.io/rogtemplate/reference/figures/ropengov-badge.svg)](https://ropengov.org/)
 [![R build
 status](https://github.com/rOpenGov/helsinki/workflows/R-CMD-check/badge.svg)](https://github.com/rOpenGov/helsinki/actions)
 [![CRAN Status
 Badge](https://www.r-pkg.org/badges/version/helsinki)](https://www.r-pkg.org/pkg/helsinki)
-[![Codecov test
-coverage](https://codecov.io/gh/rOpenGov/helsinki/branch/master/graph/badge.svg)](https://codecov.io/gh/rOpenGov/helsinki?branch=master)
 [![Downloads
 total](http://cranlogs.r-pkg.org/badges/grand-total/helsinki)](https://cran.r-project.org/package=helsinki)
 [![Downloads
@@ -23,52 +22,62 @@ GitHub](https://img.shields.io/github/stars/ropengov/helsinki.svg?style=social)]
 Twitter](https://img.shields.io/twitter/follow/ropengov.svg?style=social)](https://twitter.com/intent/follow?screen_name=ropengov)
 <!-- badges: end -->
 
-R tools to access and download open data from City of Helsinki and the
-Helsinki metropolitan area (Finnish capital region).
+The goal of helsinki package is to provide tools in R to access and
+download open data from City of Helsinki and the Helsinki metropolitan
+area - the Finnish capital region.
 
-### Installation
+## Installation
+
+You can install the released version of helsinki from
+[CRAN](https://CRAN.R-project.org) with:
 
 ``` r
-# Stable release
 install.packages("helsinki")
-# Development version
+```
+
+And the development version from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("remotes)
 library(remotes)
 remotes::install_github("ropengov/helsinki")
 ```
 
-### Using the package
+## Using the package
 
-As a simple example we load the package, list available features from
-Helsinki Region Environmental Services HSY WFS API, and then download
-the 15th feature from that list:
+Loading the package:
 
 ``` r
-library(helsinki) 
+library(helsinki)
+```
+
+List available features from Helsinki Region Environmental Services HSY
+WFS API and then download the 15th feature from that list:
+
+``` r
 url <- "https://kartta.hsy.fi/geoserver/wfs"
-
 hsy_features <- get_feature_list(base.url = url)
-
 get_feature(base.url = url, typename = hsy_features$Name[15])
 ```
 
 For more examples, check the [tutorial
 page](http://ropengov.github.io/helsinki/articles/helsinki_tutorial.html).
 
-### Contributing
+## Contributing
 
 You are welcome to contact us:
 
-  - [Submit suggestions and bug
-    reports](https://github.com/ropengov/helsinki/issues) (provide the
-    output of `sessionInfo()` and `packageVersion("helsinki")` and
-    preferably provide a [reproducible
-    example](http://adv-r.had.co.nz/Reproducibility.html))
-  - [Send a pull request](https://github.com/ropengov/helsinki/)
-  - [Star us on the Github page](https://github.com/ropengov/helsinki/)
-  - [See our website](http://ropengov.org/community/) for additional
-    contact information
+- [Submit suggestions and bug
+  reports](https://github.com/ropengov/helsinki/issues) (provide the
+  output of `sessionInfo()` and `packageVersion("helsinki")` and
+  preferably provide a [reproducible
+  example](http://adv-r.had.co.nz/Reproducibility.html))
+- [Send a pull request](https://github.com/ropengov/helsinki/)
+- [Star us on the Github page](https://github.com/ropengov/helsinki/)
+- [See our website](https://ropengov.org/community/) for additional
+  contact information
 
-### Acknowledgements
+## Acknowledgements
 
 **Kindly cite this work** as follows: [Juuso
 Parkkinen](https://github.com/ouzor), [Joona
@@ -78,5 +87,5 @@ Lahti](https://github.com/antagomir). helsinki - Helsinki open data R
 tools. URL: <http://ropengov.github.io/helsinki/>
 
 We are grateful to all
-[contributors](https://github.com/rOpenGov/helsinki/graphs/contributors)\!
-This project is part of [rOpenGov](http://ropengov.org).
+[contributors](https://github.com/rOpenGov/helsinki/graphs/contributors)!
+This project is part of [rOpenGov](https://ropengov.org).
--- inst/doc/helsinki_tutorial.R
+++ inst/doc/helsinki_tutorial.R
@@ -23,9 +23,9 @@ knitr::opts_chunk$set(
 library(helsinki)
 
 ## ----wfs1---------------------------------------------------------------------
-url <- "https://kartta.hsy.fi/geoserver/wfs"
+input_url <- "https://kartta.hsy.fi/geoserver/wfs"
 
-hsy_features <- get_feature_list(base.url = url)
+hsy_features <- get_feature_list(base.url = input_url)
 # Select only features which are related to water utilities and services
 hsy_vesihuolto <- hsy_features[which(hsy_features$Namespace == "vesihuolto"),]
 hsy_vesihuolto
@@ -33,28 +33,51 @@ hsy_vesihuolto
 feature_of_interest <- "vesihuolto:VH_Vesipostit_HSY"
 
 ## ----wfs2---------------------------------------------------------------------
+input_url <- "https://kartta.hsy.fi/geoserver/wfs"
+feature_of_interest <- "vesihuolto:VH_Vesipostit_HSY"
+
 # downloading a feature
-waterposts <- get_feature(base.url = url, typename = feature_of_interest)
+waterposts <- get_feature(base.url = input_url, typename = feature_of_interest)
+
 # Visualizing the location of waterposts
-plot(waterposts$geom)
+if (exists("waterposts")) {
+  if (!is.null(waterposts)) {
+    plot(waterposts$geom)
+  }
+}
 
 ## ----wfs3, eval = FALSE-------------------------------------------------------
+#  input_url <- "https://kartta.hsy.fi/geoserver/wfs"
+#  
 #  # Interactive example with select_feature
-#  selected_feature <- select_feature(base.url = url)
-#  feature <- get_feature(base.url = url, typename = selected_feature)
+#  selected_feature <- select_feature(base.url = input_url)
+#  feature <- get_feature(base.url = input_url, typename = selected_feature)
 #  
 #  # Skipping a redundant step with parameter get = TRUE
-#  feature <- select_feature(base.url = url, get = TRUE)
+#  feature <- select_feature(base.url = input_url, get = TRUE)
 
-## ----get_hsy_examples---------------------------------------------------------
+## ----hsy_examples-------------------------------------------------------------
+library(ggplot2)
+
 pop_grid <- get_vaestotietoruudukko(year = 2018)
 building_grid <- get_rakennustietoruudukko(year = 2020)
 
+# Logarithmic scales to make the visualizations more discernible
+if (!all(is.null(pop_grid), is.null(building_grid))) {
+  ggplot(pop_grid) + geom_sf(aes(colour=log(asukkaita), fill=log(asukkaita)))
+  ggplot(building_grid) + geom_sf(aes(colour=log(kerala_yht), fill=log(kerala_yht)))
+}
+
+## ----hsy_examples2------------------------------------------------------------
 library(ggplot2)
 
-# Logarithmic scales to make the visualizations more discernible
-ggplot(pop_grid) + geom_sf(aes(colour=log(asukkaita), fill=log(asukkaita)))
-ggplot(building_grid) + geom_sf(aes(colour=log(kerala_yht), fill=log(kerala_yht)))
+pop_grid2 <- get_vaestotietoruudukko(year = 2011)
+building_grid2 <- get_rakennustietoruudukko(year = 2011)
+
+if (!all(is.null(pop_grid2), is.null(building_grid2))) {
+  ggplot(pop_grid2) + geom_sf(aes(colour=log(ASUKKAITA), fill=log(ASUKKAITA)))
+  ggplot(building_grid2) + geom_sf(aes(colour=log(ASVALJYYS), fill=log(ASVALJYYS)))
+}
 
 ## ----servicemap, message=FALSE, warning=FALSE---------------------------------
 # Search for "puisto" (park) (specify q="query")
@@ -64,19 +87,16 @@ str(search_puisto, max.level = 1)
 
 ## ----search_example1----------------------------------------------------------
 # Get names for the first 20 results
-search_puisto$results$name$fi
+search_puisto$results$name.fi
 
 # See what kind of data is given for services
 names(search_puisto$results)
 
 ## ----search_example2----------------------------------------------------------
 search_puisto <- get_servicemap(query="search", q="puisto", page_size = 30, page = 2)
-search_puisto$results$name$fi
 
-## ----search_example3----------------------------------------------------------
-# Search for padel-related services in Helsinki
-search_padel <- get_servicemap(query="search", input="padel", only="unit.name, unit.location.coordinates, unit.street_address", municipality="helsinki")
-search_padel$results
+str(search_puisto)
+search_puisto$results$name.fi
 
 ## ----linkedevents, message=FALSE, warning=FALSE-------------------------------
 # Search for current events
@@ -86,27 +106,30 @@ events$data$name$fi
 # See what kind of data is given for events
 names(events$data)
 
-## ----mapping_example1---------------------------------------------------------
-helsinki <- get_city_map(city = "helsinki", level = "suuralue")
-espoo <- get_city_map(city = "espoo", level = "suuralue")
-vantaa <- get_city_map(city = "vantaa", level = "suuralue")
-kauniainen <- get_city_map(city = "kauniainen", level = "suuralue")
-
-library(ggplot2)
-
-ggplot() +
-  geom_sf(data = helsinki) +
-  geom_sf(data = espoo) +
-  geom_sf(data = vantaa) +
-  geom_sf(data = kauniainen) +
-  geom_sf(data = waterposts)
+## ----mapping_example1, eval = FALSE-------------------------------------------
+#  helsinki <- get_city_map(city = "helsinki", level = "suuralue")
+#  espoo <- get_city_map(city = "espoo", level = "suuralue")
+#  vantaa <- get_city_map(city = "vantaa", level = "suuralue")
+#  kauniainen <- get_city_map(city = "kauniainen", level = "suuralue")
+#  
+#  library(ggplot2)
+#  
+#  if (!all(is.null(helsinki), is.null(espoo), is.null(vantaa), is.null(kauniainen), is.null(waterposts))) {
+#    ggplot() +
+#      geom_sf(data = helsinki) +
+#      geom_sf(data = espoo) +
+#      geom_sf(data = vantaa) +
+#      geom_sf(data = kauniainen) +
+#      geom_sf(data = waterposts)
+#  }
 
 ## ----mapping_example2, eval=FALSE---------------------------------------------
-#  library(sf)
 #  map <- get_city_map(city = "helsinki", level = "suuralue")
-#  plot(sf::st_geometry(map))
-#  
 #  voting_district <- get_city_map(city = "helsinki", level = "aanestysalue")
+
+## ----mapping_example2_plot, eval=FALSE----------------------------------------
+#  library(sf)
+#  plot(sf::st_geometry(map))
 #  plot(sf::st_geometry(voting_district))
 
 ## ----hri_stats1, message=FALSE, warning=FALSE---------------------------------
@@ -115,11 +138,12 @@ stats_list <- get_hri_stats(query="")
 # Show first results
 head(stats_list)
 
-## ----hri_stats2, message=FALSE, warning=FALSE---------------------------------
-# Retrieve a specific dataset
-stats_res <- get_hri_stats(query=stats_list[1])
-# Show the structure of the results
-str(stats_res)
+if (!is.null(stats_list)) {
+  # Retrieve a specific dataset
+  stats_res <- get_hri_stats(query=stats_list[1])
+  # Show the structure of the results
+  str(stats_res)
+}
 
 ## ----citation, comment=NA-----------------------------------------------------
 citation("helsinki")
